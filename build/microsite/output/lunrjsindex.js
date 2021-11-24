f51f0a4a7ed6f4e0e3dbd61ef6cd02c458f92af5
Ralf D. Müller <ralf.d.mueller@gmail.com>
2021-11-24 22:44:36 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- build/microsite/output/lunrjsindex.js
+++ build/microsite/output/lunrjsindex.js
@@ -10,6 +10,14 @@ var documents = [
 
 {
     "id": 1,
+    "uri": "demo/markdown.html",
+    "menu": "null",
+    "title": "Weekly Links #2",
+    "text": " Markdown Demo Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. "
+},
+
+{
+    "id": 2,
     "uri": "search.html",
     "menu": "-",
     "title": "search",
@@ -17,7 +25,7 @@ var documents = [
 },
 
 {
-    "id": 2,
+    "id": 3,
     "uri": "lunrjsindex.html",
     "menu": "-",
     "title": "null",
